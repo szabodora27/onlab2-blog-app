e51deb179b284276491afe941cba4f38b610e7c9
Szabó Dóra <szabodora27@gmail.com>
2018-09-20 20:32:52 +0200

    Create the project's hierarchy

--- Blog.Dal/ApplicationDbContext.cs
+++ Blog.Dal/ApplicationDbContext.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Blog.Data
+namespace Blog.Bll
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Blog.Dal/Migrations/20180920174542_Init.cs
+++ Blog.Dal/Migrations/20180920174542_Init.cs
@@ -2,9 +2,9 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Blog.Data.Migrations
+namespace Blog.Dal.Migrations
 {
-    public partial class CreateIdentitySchema : Migration
+    public partial class Init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
